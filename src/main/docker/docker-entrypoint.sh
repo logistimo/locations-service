fcfedd6f93712cb8bdef40105d53b5855d9a9b94
Narenlogi <Narenlogi@users.noreply.github.com>
2017-06-30 11:38:11 +0530

    Updated the docker files for sentinel (#3)

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -9,7 +9,10 @@ exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
     -Dspring.location.db.password=$MYSQL_LOC_PASS \
     -Dspring.logistimo.db.username=$MYSQL_LOGI_USER \
     -Dspring.logistimo.db.password=$MYSQL_LOGI_PASS \
-    -Dspring.redis.host=$REDIS_HOST \
+    -Dapp.redis.host=$REDIS_HOST \
+    -Dapp.redis.port=$REDIS_PORT \
     -Dapp-data.start=$APP_START \
-    -javaagent:/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:/jmx_exporter.json \
-    -jar /location-service.jar
+    -Dapp.redis.sentinel.master=$SENTINEL_MASTER \
+    -Dapp.redis.sentinel.nodes=$SENTINEL_HOST \
+    -javaagent:$HOME/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:$HOME/jmx_exporter.json \
+    -jar $HOME/location-service.jar
