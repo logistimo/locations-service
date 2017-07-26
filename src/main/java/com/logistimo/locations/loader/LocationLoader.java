5c6a98c5d4d2804c1b3c4ac543bd8b2bba7685e5
kgaurav1 <kumar@logistimo.com>
2017-07-26 15:24:15 +0530

    Refactor Myanmar (#9)
    * Changes made for cache implementation (env wise : see condition in RedisConfiguration class)
    
    * pom.xml changes
    
    * refactored
    
    * refactored
    
    * refactored
    
    * refactored
    
    * refactored for Myanamar

--- src/main/java/com/logistimo/locations/loader/LocationLoader.java
+++ src/main/java/com/logistimo/locations/loader/LocationLoader.java
@@ -1,3 +1,5 @@
+
+
 package com.logistimo.locations.loader;
 
 import com.google.gson.JsonArray;
@@ -24,129 +26,127 @@ import java.util.*;
 @Component
 public class LocationLoader {
 
-  final static String NAME = "name";
-  final static String STATES = "states";
-  final static String COMMA = ",";
+    final static String NAME = "name";
+    final static String STATES = "states";
+    final static String TALUK = "taluks";
+    final static String USER = "location-user";
 
-  @Resource
-  CountryRepository repository;
+    @Resource
+    CountryRepository repository;
 
 
-  public void load() throws IOException {
+    public void load() throws IOException {
 
-    org.springframework.core.io.Resource resource = new ClassPathResource("location.json");
-    Reader fileReader = new InputStreamReader(resource.getInputStream());
-    JsonParser parser = new JsonParser();
-    JsonObject countries = parser.parse(fileReader).getAsJsonObject().get("data").getAsJsonObject();
-    Iterator itr = countries.entrySet().iterator();
+        org.springframework.core.io.Resource resource = new ClassPathResource("location.json");
+        Reader fileReader = new InputStreamReader(resource.getInputStream());
+        JsonParser parser = new JsonParser();
+        JsonObject countries = parser.parse(fileReader).getAsJsonObject().get("data").getAsJsonObject();
+        Iterator itr = countries.entrySet().iterator();
 
-    //Country
-    Country country = null;
-    Set<State> states = null;
-    while (itr.hasNext()) {
+        //Country
+        Country country = null;
+        Set<State> states = null;
+        while (itr.hasNext()) {
 
-      country = new Country();
-      states = new HashSet<>();
-      String ccode = null;
-      JsonObject cobject = null;
-      Map.Entry<String, JsonElement> entry = (Map.Entry<String, JsonElement>) itr.next();
-      ccode = entry.getKey();
-      cobject = entry.getValue().getAsJsonObject();
-      Iterator citr = cobject.entrySet().iterator();
-      String cname = null;
-      while (citr.hasNext()) {
+            country = new Country();
+            states = new HashSet<>();
+            String ccode = null;
+            JsonObject cobject = null;
+            Map.Entry<String, JsonElement> entry = (Map.Entry<String, JsonElement>) itr.next();
+            ccode = entry.getKey();
+            cobject = entry.getValue().getAsJsonObject();
+            Iterator citr = cobject.entrySet().iterator();
+            String cname = null;
+            while (citr.hasNext()) {
 
-        Map.Entry<String, JsonElement> centry = (Map.Entry<String, JsonElement>) citr.next();
-        JsonObject sobject = null;
-        Iterator sitr = null;
-        String sname = null;
-        JsonObject dobject = null;
-        if (NAME.equals(centry.getKey()) && centry.getValue().isJsonPrimitive()) {
-          cname = centry.getValue().getAsString();
-          country.setCode(ccode);
-          country.setName(cname);
-          country.setCreatedBy("test-user");
-          country.setCreatedOn(new Date());
-          //System.out.println("[Country with code " + ccode + "and name " + cname + "]");
-        }
-
-        if (STATES.equals(centry.getKey()) && centry.getValue().isJsonObject()) {
-          if (null != cname && ("Switzerland".equals(cname) || "Myanmar".equals(cname))) {
-            continue;
-          }
-          sobject = centry.getValue().getAsJsonObject();
-          sitr = sobject.entrySet().iterator();
-          State state = null;
-          while (sitr.hasNext()) {
-            Map.Entry<String, JsonElement> sentry = (Map.Entry<String, JsonElement>) sitr.next();
-            sname = sentry.getKey();
-            state = new State();
-            state.setName(sname);
-            state.setCreatedBy("test-user");
-            state.setCreatedOn(new Date());
-            state.setCountry(country);
-            states.add(state);
-//            System.out.println("[State " + sname + " is in counttry " + cname + "]");
-            dobject =
-                sentry.getValue().getAsJsonObject().entrySet().iterator().next().getValue()
-                    .getAsJsonObject();
-            Iterator ditr = dobject.entrySet().iterator();
-            String dname = null;
-            District district = null;
-            Set<District> districts = new HashSet<>();
-            while (ditr.hasNext()) {
-              Map.Entry<String, JsonElement> dentry = (Map.Entry<String, JsonElement>) ditr.next();
-              dname = dentry.getKey();
-//              System.out.println("[District " + dname + " is in state " + sname + "]");
-              district = new District();
-              district.setName(dname);
-              district.setCreatedBy("test-user");
-              district.setCreatedOn(new Date());
-              district.setState(state);
-
-              Set<SubDistrict> subDistricts = null;
-              if (dentry.getValue().getAsJsonObject() != null
-                  && dentry.getValue().getAsJsonObject().entrySet().size() > 0) {
-                JsonArray
-                    tarray =
-                    dentry.getValue().getAsJsonObject().entrySet().iterator().next().getValue()
-                        .getAsJsonArray();
-                subDistricts = populateSubDistricts(district,tarray);
-//                System.out.println("[District " + dname + " with taluks [" + iteateOverStrList(
-//                    convertToStrList(tarray)) + "]");
-                if(subDistricts.size() >0) {
-                  district.setSubDistricts(subDistricts);
+                Map.Entry<String, JsonElement> centry = (Map.Entry<String, JsonElement>) citr.next();
+                JsonObject sobject = null;
+                Iterator sitr = null;
+                String sname = null;
+                JsonObject dobject = null;
+                if (NAME.equals(centry.getKey()) && centry.getValue().isJsonPrimitive()) {
+                    cname = centry.getValue().getAsString();
+                    country.setCode(ccode);
+                    country.setName(cname);
+                    country.setCreatedBy(USER);
+                    country.setCreatedOn(new Date());
                 }
-              }
-              districts.add(district);
+
+                if (STATES.equals(centry.getKey()) && centry.getValue().isJsonObject()) {
+                    if (null != cname && ("Switzerland".equals(cname))) {
+                        continue;
+                    }
+                    sobject = centry.getValue().getAsJsonObject();
+                    sitr = sobject.entrySet().iterator();
+                    State state = null;
+                    while (sitr.hasNext()) {
+                        Map.Entry<String, JsonElement> sentry = (Map.Entry<String, JsonElement>) sitr.next();
+                        sname = sentry.getKey();
+                        state = new State();
+                        state.setName(sname);
+                        state.setCreatedBy(USER);
+                        state.setCreatedOn(new Date());
+                        state.setCountry(country);
+                        states.add(state);
+                        dobject =
+                                sentry.getValue().getAsJsonObject().entrySet().iterator().next().getValue()
+                                        .getAsJsonObject();
+                        Iterator ditr = dobject.entrySet().iterator();
+                        String dname = null;
+                        District district = null;
+                        Set<District> districts = new HashSet<>();
+                        while (ditr.hasNext()) {
+                            Map.Entry<String, JsonElement> dentry = (Map.Entry<String, JsonElement>) ditr.next();
+                            dname = dentry.getKey();
+                            district = new District();
+                            district.setName(dname);
+                            district.setCreatedBy(USER);
+                            district.setCreatedOn(new Date());
+                            district.setState(state);
+
+                            Set<SubDistrict> subDistricts = null;
+                            if (dentry.getValue().getAsJsonObject() != null
+                                    && dentry.getValue().getAsJsonObject().entrySet().size() > 0) {
+                                if (dentry.getValue().getAsJsonObject().entrySet().iterator().next().getKey().equalsIgnoreCase(TALUK)) {
+                                    JsonArray
+                                            tarray =
+                                            dentry.getValue().getAsJsonObject().entrySet().iterator().next().getValue()
+                                                    .getAsJsonArray();
+                                    subDistricts = populateSubDistricts(district, tarray);
+                                    if (subDistricts.size() > 0) {
+                                        district.setSubDistricts(subDistricts);
+                                    }
+                                }
+                            }
+                            districts.add(district);
+                        }
+                        if (districts.size() > 0)
+                            state.setDistricts(districts);
+                    }
+                }
             }
-            if (districts.size() >0)
-              state.setDistricts(districts);
-          }
+            if (states.size() > 0)
+                country.setStates(states);
+            //saving to database
+            repository.save(country);
         }
-      }
-      if(states.size() >0)
-        country.setStates(states);
-      //saving to database
-      repository.save(country);
+
     }
 
-  }
+    Set<SubDistrict> populateSubDistricts(District district, JsonArray jArray) {
 
-  Set<SubDistrict> populateSubDistricts(District district,JsonArray jArray) {
-
-    Set<SubDistrict> subDistricts = new HashSet<>();
-    SubDistrict subDistrict = null;
-    if (jArray != null) {
-      for (int i = 0; i < jArray.size(); i++) {
-        subDistrict = new SubDistrict();
-        subDistrict.setName(jArray.get(i).getAsString());
-        subDistrict.setCreatedBy("test-user");
-        subDistrict.setCreatedOn(new Date());
-        subDistrict.setDistrict(district);
-        subDistricts.add(subDistrict);
-      }
+        Set<SubDistrict> subDistricts = new HashSet<>();
+        SubDistrict subDistrict = null;
+        if (jArray != null) {
+            for (int i = 0; i < jArray.size(); i++) {
+                subDistrict = new SubDistrict();
+                subDistrict.setName(jArray.get(i).getAsString());
+                subDistrict.setCreatedBy(USER);
+                subDistrict.setCreatedOn(new Date());
+                subDistrict.setDistrict(district);
+                subDistricts.add(subDistrict);
+            }
+        }
+        return subDistricts;
     }
-    return subDistricts;
-  }
 }
