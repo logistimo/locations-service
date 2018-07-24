e18b903b702341bd6f3d4a590502dff305390700
narendiran <naren@logistimo.com>
2018-07-24 10:38:38 +0530

    Updated the Dockerfiles for apm-agent-version

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-wget -P $HOME/ http://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$APM_AGENT_VER/elastic-apm-agent-$APM_AGENT_VER.jar
+
 
 exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
     -Dserver.port=$SERVER_PORT \
@@ -17,7 +17,7 @@ exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
     -Dapp.redis.sentinel.master=$SENTINEL_MASTER \
     -Dapp.redis.sentinel.nodes=$SENTINEL_HOST \
     -javaagent:$HOME/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:$HOME/jmx_exporter.json \
-    -javaagent:$HOME/elastic-apm-agent-$APM_AGENT_VER.jar \
+    -javaagent:$HOME/elastic-apm-agent-0.6.0.jar \
 	-Delastic.apm.service_name=$SERVICE_NAME \
     -Delastic.apm.application_packages=com.logistimo.locations \
     -Delastic.apm.server_url=http://$APM_SERVER_URL \
